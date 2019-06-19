f5be3b02b04c82dfab7cd968af18060a7e96ad9c
SergeiGray <borodat_for@mail.ru>
2019-06-20 00:51:02 +0300

    Начало разметки блока payment

--- handler.php
+++ handler.php
@@ -1,6 +1,9 @@
 <html>
 <body>
 
+Email: <?php echo $_POST["email"]; ?><br>
+Имя: <?php echo $_POST["name"]; ?><br>
+Текст: <?php echo $_POST["text"]; ?><br>
 Тип: <?php echo $_POST["type"]; ?><br>
 Страна: <?php echo $_POST["country"]; ?><br>
 Количество: <?php echo $_POST["amount"]; ?><br>
