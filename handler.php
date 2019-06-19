2fbb1771dd1df509257163170f950141f3e0555a
SergeiGray <borodat_for@mail.ru>
2019-06-19 18:16:10 +0300

    Разметка/стили блока wholesale

---
+++ handler.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+
+Тип: <?php echo $_POST["type"]; ?><br>
+Страна: <?php echo $_POST["country"]; ?><br>
+Количество: <?php echo $_POST["amount"]; ?><br>
+Срок: <?php echo $_POST["time"]; ?><br>
+Протокол: <?php echo $_POST["protocol"]; ?><br>
+Адрес: <?php echo $_POST["address"]; ?>
+
+</body>
+</html>
